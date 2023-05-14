84f18a2ee775290f8752eab38356f0605ab7bf22
Kapil Chaudhary <kc685838@gmail.com>
2023-05-14 14:20:38 +0530

    submitting code-Sun May 14 2023 14:20:35 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -7,31 +7,19 @@ const App = () => {
   const [shirtSize, setShirtSize] = useState("");
   const [dressSize, setDressSize] = useState("");
 
-  const handleGenderChange = (event) => {
-    setGender(event.target.value);
-  };
-
-  const handleShirtSizeChange = (event) => {
-    setShirtSize(event.target.value);
-  };
-
-  const handleDressSizeChange = (event) => {
-    setDressSize(event.target.value);
-  };
-
   return (
     <div>
         {/* Do not remove the main div */}
         <h2 htmlFor="gender">Select your gender:</h2>
-        <input type="radio" name="gender" onClick={handleGenderChange} value="Male" checked/>
+        <input type="radio" name="gender" onChange={(e) => {setGender(e.target.value);}} value="Male" checked/>
         <label for="male">Male</label>
-        <input type="radio" name="gender" onClick={handleGenderChange} value="Female" />
+        <input type="radio" name="gender" onChange={(e) => {setGender(e.target.value);}} value="Female" />
         <label for="female">Female</label>
 
         {gender === "Male" && (
           <div>
             <h2 htmlFor="shirtSize">Select your shirt size:</h2>
-            <select id="shirtSize" name="shirtSize" onChange={handleShirtSizeChange}>
+            <select id="shirtSize" name="shirtSize" onChange={(e) => {setShirtSize(e.target.value)}}>
               <option value="Select size" disabled>Select size</option>
               <option value="Small">Small</option>
               <option value="Medium">Medium</option>
@@ -43,7 +31,7 @@ const App = () => {
         {gender === "Female" && (
           <div>
             <h2 htmlFor="dressSize">Select your dress size:</h2>
-            <select id="dressSize" name="dressSize" onChange={handleDressSizeChange}>
+            <select id="dressSize" name="dressSize" onChange={(e) => {setDressSize(e.target.value)}}>
             <option value="Select size" disabled>Select size</option>
               <option value="2">2</option>
               <option value="4">4</option>
